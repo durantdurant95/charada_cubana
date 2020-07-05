788c2500305c1589a4d5c61ddd57131f91225ac8
durantdurant95 <apduran95@gmail.com>
2020-07-05 11:17:47 -0400

    Updates

---
+++ precache-manifest.0b0306ee447013b2bfae30edcd6ea3f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6207c092d408bb127c94e0b1054af5be",
+    "url": "/charada_cubana/index.html"
+  },
+  {
+    "revision": "29ac123e933e2ac0ee4d",
+    "url": "/charada_cubana/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e2ab5752fd79ad8cb936",
+    "url": "/charada_cubana/static/css/main.b4950c52.chunk.css"
+  },
+  {
+    "revision": "29ac123e933e2ac0ee4d",
+    "url": "/charada_cubana/static/js/2.2e6584ef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/charada_cubana/static/js/2.2e6584ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ab5752fd79ad8cb936",
+    "url": "/charada_cubana/static/js/main.fd6c9d82.chunk.js"
+  },
+  {
+    "revision": "8a813e8082c0f4b11b96",
+    "url": "/charada_cubana/static/js/runtime-main.79e78131.js"
+  }
+]);
