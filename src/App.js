c76caa5c75121698f6edbd164072cedddf0386c4
durantdurant95 <apduran95@gmail.com>
2020-07-05 22:18:26 -0400

    Severos cambios en la interfaz y limpiado de la busqueda

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import Listado from "./components/Listado";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Inicio from "./components/Inicio";
+import Buscador from "./components/Buscador";
+import Charada from "./components/Charada";
+import Footer from "./components/Footer";
 import "./App.scss";
+import BarraNav from "./components/BarraNav";
 
 function App() {
   return (
-    <Container>
-      <h1 className="text-center my-3">Charada Cubana</h1>
-      <h3 className="text-center">
-        Simple buscador de resultados en la conocida charada cubana, suerte!!!
-      </h3>
-      <p className="text-center my-4">
-        Puede buscar tanto por números como por palabras clave y obtendrá los
-        resultados
-      </p>
-      <Listado />
-    </Container>
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <BarraNav />
+            <Inicio />
+            <Footer />
+          </Route>
+          <Route path="/buscador" exact>
+            <BarraNav />
+            <Buscador />
+          </Route>
+          <Route path="/charada" exact>
+            <BarraNav />
+            <Charada />
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
